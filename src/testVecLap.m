9a7cac5fe540331e6877e690f952503d8a6de10e
Dave <sanchda@gmail.com>
2013-01-25 16:30:04 -0700

    VecLap almost done
    For some reason, U(:,2) is coming out wonky.

--- src/testVecLap.m
+++ src/testVecLap.m
@@ -1,4 +1,4 @@
-function [ Maxerr, L2err, kappa ] = testVecLap( x, W, U0, eps )
+function [ Maxerr, L2err, kappa ] = testVecLap( x, W, U0, epsilon )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -10,15 +10,13 @@ function [ Maxerr, L2err, kappa ] = testVecLap( x, W, U0, eps )
     
     % Parameter to all the RBF calls.  This parameter will have to be
     % changed if a different RBF kernel is used.
-    twoeps2 = 2*eps*eps;
+    twoeps2 = 2*epsilon*epsilon;
    
 
     % Build the distance matrix.  Simplification is due to the centers
-   
     % being on the surface of the sphere.
-    
     r2 = 2*(1 - x(:,1)*x(:,1).' - x(:,2)*x(:,2).' - x(:,3)*x(:,3).');
-    r2 = eps*eps*r2;
+    r2 = epsilon*epsilon*r2;
     A = exp(-r2);
     
     kappa = cond(A);
@@ -104,22 +102,44 @@ function [ Maxerr, L2err, kappa ] = testVecLap( x, W, U0, eps )
     X = diag(x(:,1));
     Y = diag(x(:,2));
     Z = diag(x(:,3));
-  
-    lapux = -Z*Lz + Y*Y*Lzz - Y*Ly - 2*Y*Z*Lyz + 2*Z*Z*Lyy;
+ 
+% attempt 1  
+    lapux = -Z*Lz + Y*Y*Lzz - Y*Ly - 2*Y*Z*Lyz + Z*Z*Lyy;
     lapuy = -X*Y*Lzz + X*Z*Lyz + Y*Lx + Y*Z*Lxz - Z*Z*Lxy;
     lapuz =  X*Y*Lyz - X*Z*Lyy + Z*Lx - Y*Y*Lxz + Y*Z*Lxy;
+
+    lapvx = X*Ly - X*Y*Lzz + X*Z*Lyz + Y*Z*Lxz - Z*Z*Lxy;
+    lapvy = -X*Lx + X*X*Lzz - Z*Lz - 2*X*Z*Lxz + Z*Z*Lxx;
+    lapvz = -X*X*Lyz + X*Y*Lxz + Z*Ly + X*Z*Lxy - Y*Z*Lxx;
     
-    lapvx =  X*Ly + X*Z*Lyz - X*Y*Lyy + Y*Z*Lxz + Z*Z*Lxy;
-    lapvy = -Z*Lz + X*X*Lzz - X*Lx - 2*X*Z*Lx + Z*Z*Lxx;
-    lapvz =  Z*Ly - X*X*Lyz + X*Y*Lxz + X*Z*Lxy - Y*Z*Lxx;
+
+    lapwx =  X*Lz + X*Y*Lyz - X*Z*Lyy - Y*Y*Lxz + Y*Z*Lxy;
+    lapwy =  Y*Lz - X*X*Lyz + X*Y*Lxz + X*Z*Lxy - Y*Z*Lxx;
+    lapwz = -Y*Ly + X*X*Lyy - X*Lx - 2*X*Y*Lxy + Y*Y*Lxx;
+
+% attempt 2
+%    lapux = -Y*Ly - Y*Y*Lzz + Z*Lz + 2*Y*Z*Lyz - Z*Z*Lyy;
+%    lapuy = -Y*Lx + X*Y*Lzz - Z*X*Lyz - Y*Z*Lxz + Z*Z*Lxy;
+%    lapuz = -X*Y*Lyz + Y*Y*Lxz - Z*Lx + Z*X*Lyy - Z*Y*Lxy;
     
-    lapwx =  X*Lz + X*Y*Lyz - X*Z*Lyy + Y*Y*Lxz + Y*Z*Lxy;
-    lapwy =  Y*Lz + X*X*Lyz + X*Y*Lxz + X*Z*Lxy - Y*Z*Lxx;
-    lapwz = -Y*Ly + X*X*Lyy - X*Lx + 2*X*Y*Lxy + Y*Y*Lxx;
-       
+%    lapvx = -X*Ly + X*Y*Lzz - Z*X*Lyz - Z*Y*Lxz + Z*Z*Lxy;
+%    lapvy = X*Lx - X*X*Lzz + Z*Lz + 2*X*Z*Lxz - Z*Z*Lxx;
+%    lapvz = X*X*Lyz - X*Y*Lxz - Z*Ly - X*Z*Lxy + Y*Z*Lxx;
+    
+%    lapwx = -X*Lz - X*Y*Lyz + Y*Y*Lxz + X*Z*Lyy - Y*Z*Lxy;
+%    lapwy = X*X*Lyz - Y*Lz - X*Y*Lxz - X*Z*Lxy + Y*Z*Lxx;
+%    lapwz = X*Lx - X*X*Lyy + Y*Ly + 2*X*Y*Lxy - Y*Y*Lxx;
+
+% attempt 3
+%    lapux = -Y*Z*Lxz + X*Y*Lz + Y*X*X*Lxz + X*Y*Y*Lyz + Z*Lxy - X*Z*Ly - X*X*Z*Lxy - X*Y*Z*Lyy + X*Y*Z*Lzz - X*Z*Z*Lyz;
+%    lapuy = Y*Lyz - Y*Y*Lz - X*Y*Y*Lxz - Y*Y*Y*Lyz - Z*Lyy + X*Z*Lx + 2*Y*Z*Ly + X*Y*Z*Lxy + Y*Y*Z*Lyy - Z*Y*Y*Lzz + Z*Z*Lz + Z*Z*Lyz;
+%    lapuz = Y*Lzz - X*Y*Lx - Y*Y*Ly - Z*Lyz - 2*Y*Z*Lz - X*Y*Z*Lxz - Y*Y*Z*Lyz + Z*Z*Ly + X*Z*Z*Lxy + Y*Z*Z*Lyy - Y*Z*Z*Lzz - Z*Z*Z*Lyz;
+
     % Now apply the vector Laplacian to the input data.
     % 
-    U = [(lapux*U0(:,1)+lapuy*U0(:,2)+lapuz*U0(:,3)) (lapvx*U0(:,1)+lapvy*U0(:,2)+lapvz*U0(:,3)) (lapwx*U0(:,1)+lapwy*U0(:,2)+lapwz*U0(:,3))];
+    U = -[(lapux*U0(:,1)+lapuy*U0(:,2)+lapuz*U0(:,3)) (lapvx*U0(:,1)+lapvy*U0(:,2)+lapvz*U0(:,3)) (lapwx*U0(:,1)+lapwy*U0(:,2)+lapwz*U0(:,3))];
+    
+%    U = [(lapux*U0(:,1)+lapvx*U0(:,2)+lapwx*U0(:,3)) (lapuy*U0(:,1)+lapvy*U0(:,2)+lapwy*U0(:,3)) (lapuz*U0(:,1)+lapvz*U0(:,2)+lapwz*U0(:,3))];
 
     % Figure out the L-infty residual
     err = abs(U0 - U);
--- src/veclapdriver.m
+++ src/veclapdriver.m
@@ -1,8 +1,9 @@
 % cd 'C:\Users\david\Desktop\GitHub\RBF-NS\src';
 cd 'C:\Users\david\Documents\GitHub\RBF-NS\src';
             
+eps = @(N) -1.37119 + 0.556389*sqrt(N);
 epsilon = 2.05;    % Shape paramater for the RBF kernel
-N   = 7;           % Somehow related to the number of centers.  For the ME points,
+N   = 10;           % Somehow related to the number of centers.  For the ME points,
                    % the number of centers is (N+1)^2.
 
                  
@@ -12,7 +13,8 @@ N   = 7;           % Somehow related to the number of centers.  For the ME point
  epsmat    = zeros(70-7);
  
  %for N = 7:70
-    
+ 
+    epsilon = eps(N);
     disp(N);
     X = getMEPoints(N);
     W = X(:,4);
@@ -24,21 +26,12 @@ N   = 7;           % Somehow related to the number of centers.  For the ME point
     for i = 1:(N+1)^2
         X(i,:) = (theta*X(i,:)')';
     end
-
     
     % A div-free VSH.  Check VecSphHarm.nb for details
-    U1 = [X(:,1).*X(:,2) (X(:,3).^2 - X(:,2).^2) X(:,2).*X(:,3)];
-
+    U1 = [X(:,1).*X(:,2) (X(:,3).^2 - X(:,2).^2) -X(:,2).*X(:,3)];
     
     % Run the test!
-    kappa = 10^5;
-    while kappa > 10^4
-        [maxerr l2err kappa] = testVecLap(X, W, U1, epsilon);
-        if kappa > 10^4
-            epsilon = 1.05*epsilon;
-            disp(epsilon);
-        end
-    end
+    [maxerr l2err kappa] = testVecLap(X, W, U1, epsilon);
 
     disp([maxerr l2err kappa])
     errinfmat(N-6) = maxerr;
